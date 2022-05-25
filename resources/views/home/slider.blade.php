d69a0f3b9cc0c9899a657db53a935173900668fc
beyzaltintas <beyzaltintas3@gmail.com>
2022-05-25 14:45:09 +0300

    Home Page Dynamic Slider

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -1,14 +1,16 @@
 
 <!-- slider_area_start -->
+
 <div class="slider_area">
     <div class="slider_active owl-carousel">
-        <div class="single_slider  d-flex align-items-center slider_bg_1 black_overlay">
+        @foreach($sliderdata as $rs)
+        <div class="swiper-slide d-flex align-items-center ">
+            <img src="{{Storage::url($rs->image)}}" alt="" >
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-xl-12 col-md-12">
                         <div class="slider_text text-center ">
-                            <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s" >Hi, This is Halen, a professional Photographer <br>
-                                I Captured Moments</h3>
+                            <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s" >{{$rs->title}}</h3>
                             <div class="video_service_btn wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s">
                                 <a href="#" class="boxed-btn3">Explore Work</a>
                             </div>
@@ -16,37 +18,8 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="single_slider  d-flex align-items-center slider_bg_1 black_overlay">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-12 col-md-12">
-                        <div class="slider_text text-center ">
-                            <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s" >Hi, This is Halen, a professional Photographer <br>
-                                I Captured Moments</h3>
-                            <div class="video_service_btn wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s">
-                                <a href="#" class="boxed-btn3">Explore Work</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
+        @endforeach
         </div>
-        <div class="single_slider  d-flex align-items-center slider_bg_1 black_overlay">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-12 col-md-12">
-                        <div class="slider_text text-center ">
-                            <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s" >Hi, This is Halen, a professional Photographer <br>
-                                I Captured Moments</h3>
-                            <div class="video_service_btn wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s">
-                                <a href="#" class="boxed-btn3">Explore Work</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
-    </div>
-</div>
 <!-- slider_area_end -->
